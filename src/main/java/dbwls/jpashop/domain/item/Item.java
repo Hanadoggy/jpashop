49bf0eb594e04d2592e3f6d8c66db0aa60fbc387
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-01-20 18:15:45 +0900

    주문 도메인 개발
    test, OrderRepository에 findAll querydsl로 예

--- src/main/java/dbwls/jpashop/domain/item/Item.java
+++ src/main/java/dbwls/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package dbwls.jpashop.domain.item;
 
 import dbwls.jpashop.domain.Category;
+import dbwls.jpashop.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,9 +21,28 @@ public abstract class Item {
 
     private String name;
     private int price;
-    private int stackQuantity;
+    private int stockQuantity;
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
---
+++ src/main/java/dbwls/jpashop/repository/OrderSearch.java
@@ -0,0 +1,14 @@
+package dbwls.jpashop.repository;
+
+import dbwls.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+
+
+}
